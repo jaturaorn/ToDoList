ceb175c157927207c704d36a24b1846d1f9d51f7
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-24 23:12:59 +0700

    Do in JS

---
+++ app.js
@@ -0,0 +1,12 @@
+const data = new Date();
+
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+document.getElementById("date").innerHTML = today.toDateString();
